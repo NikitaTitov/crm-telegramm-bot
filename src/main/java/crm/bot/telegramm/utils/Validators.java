a8c74927fc15acfd151a91c81be16120a93291bc
IvanCherepica <pivan1996@yandex.ru>
2018-01-19 13:38:32 +0300

    change max price amount

--- src/main/java/crm/bot/telegramm/utils/Validators.java
+++ src/main/java/crm/bot/telegramm/utils/Validators.java
@@ -5,7 +5,7 @@ public class Validators {
 	public static boolean isValidPrice(String price) {
 		try {
 			Long longPrice = Long.parseLong(price);
-			if (Math.ceil(Math.log10(longPrice + 0.5)) < 4) {
+			if (Math.ceil(Math.log10(longPrice + 0.5)) < 7) {
 				return true;
 			}
 		} catch (NumberFormatException ignored) {
